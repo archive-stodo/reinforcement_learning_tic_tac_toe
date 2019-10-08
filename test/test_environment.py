6403740d2bbbf4c10c9a992eebe5b900d2505606
dawid <stodoo@gmail.com>
2019-10-09 01:02:58 +0200

    hashing state number and tests

--- test/test_environment.py
+++ test/test_environment.py
@@ -8,12 +8,38 @@ class TestEnvironment(unittest.TestCase):
         env = Environment(4, 3)
 
         # when
-        env.set_x(0, 0) # x denoted as -1 in numpy array
-        env.set_o(0, 1) # o denoted as 1 in numpy array
+        env.set_x(0, 0) # x denoted as 1 in numpy array
+        env.set_o(0, 1) # o denoted as 2 in numpy array
 
         # then
         self.assertEqual(env.rows, 4)
         self.assertEqual(env.columns, 3)
 
-        self.assertEqual(env.board[0, 0], -1)
-        self.assertEqual(env.board[0, 1], 1)
+        self.assertEqual(env.board[0, 0], 1)
+        self.assertEqual(env.board[0, 1], 2)
+
+    def test_get_state_number(self):
+        env = Environment(3, 3)
+        env.set_x(0, 0) # 1
+        env.set_x(0, 1) # 3
+        env.set_x(0, 2) # 9
+        env.set_o(1, 1) # 3^4 * 2 = 162
+        state_number = env.get_state_number()
+
+        self.assertEqual(175, state_number)
+
+    def test_3x3_max_number_for_get_state_number(self):
+        env = Environment(3, 3)
+        env.board.fill(2)
+
+        state_number = env.get_state_number()
+
+        self.assertEqual(3 ** 9 - 1, state_number)
+
+    def test_4x4_max_number_for_get_state_number(self):
+        env = Environment(4, 4)
+        env.board.fill(2)
+
+        state_number = env.get_state_number()
+
+        self.assertEqual(3 ** 16 - 1, state_number)
--- tictactoe/environment.py
+++ tictactoe/environment.py
@@ -11,7 +11,24 @@ class Environment:
         self.num_states = 3**(rows*columns)
 
     def reward(self, player_symbol):
-        pass
+        if not self.is_game_over():
+            return 0
+
+        return 1 if self.winner == player_symbol else 0
+
+    def is_empty(self, i, j):
+        return self.board[i, j] == 0
+
+    def get_state_number(self):
+        power = 0
+        state_number = 0
+        for row in range(len(self.board)):
+            for column in range(len(self.board[0])):
+                current_field_value = self.board[row][column] # 0 1 2
+                state_number += 3**power * current_field_value
+                power += 1
+
+        return state_number
 
     def initialV_x(env, state_winner_triples):
         pass
@@ -19,17 +36,24 @@ class Environment:
     def initialV_o(env, state_winner_triples):
         pass
 
-    def is_game_over(self):
+    def is_game_won(self):
         pass
+        #check rows
+
+        #check columns
+
+        #check diagonals
+
+
 
     def is_draw(self):
         pass
 
     def set_x(self, row, column):
-        self.board[row, column] = -1
+        self.board[row, column] = 1
 
     def set_o(self, row, column):
-        self.board[row, column] = 1
+        self.board[row, column] = 2
 
     def print_array(self):
         for row in range(len(self.board)):
@@ -37,8 +61,8 @@ class Environment:
                 value = self.board[row][column]
                 if value == 0:
                     print('.', end=" ")
-                elif value == -1:
-                    print('x', end=" ")
                 elif value == 1:
+                    print('x', end=" ")
+                elif value == 2:
                     print('o', end=" ")
             print()
